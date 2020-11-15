bdd675b9e140bb0f2bb9fb4c4441e5cc96ba678f
Ian Helmrich <55527600+lordi-byte@users.noreply.github.com>
2020-11-15 16:38:37 +0100

    Add first user authentication

--- android_app/app/src/main/java/com/example/fitcom/MainActivity.kt
+++ android_app/app/src/main/java/com/example/fitcom/MainActivity.kt
@@ -1,26 +1,37 @@
 package com.example.fitcom
 
 import android.os.Bundle
+import android.widget.TextView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import kotlinx.coroutines.delay
+import java.util.logging.Handler
 
 class MainActivity : AppCompatActivity() {
+    var user_authn: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        setContentView(R.layout.loading_screen)
+        //Thread.sleep(10000)
+        if(user_authn) {
+            val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
+            val navController = findNavController(R.id.nav_host_fragment)
+            // Passing each menu ID as a set of Ids because each
+            // menu should be considered as top level destinations.
+            val appBarConfiguration = AppBarConfiguration(setOf(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+            setupActionBarWithNavController(navController, appBarConfiguration)
+            navView.setupWithNavController(navController)
+        }
+        else {
+            //Start activity for registration
+            findViewById<TextView>(R.id.loading_screen).text = "you have to sign up"
+        }
     }
 }
