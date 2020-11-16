b4368187304b6ae6896a70f6994f537b0c23d392
Ian Helmrich <55527600+lordi-byte@users.noreply.github.com>
2020-11-16 18:48:09 +0100

    add fragment navigation

--- android_app/app/src/main/java/com/example/fitcom/QRInstructions.kt
+++ android_app/app/src/main/java/com/example/fitcom/QRInstructions.kt
@@ -5,55 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [QRInstructions.newInstance] factory method to
- * create an instance of this fragment.
- */
 class QRInstructions : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_qr_instructions, container, false)
-    }
+        val root = inflater.inflate(R.layout.fragment_qr_instructions, container, false)
+        val scan =  root.findViewById<Button>(R.id.scan)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment QRInstructions.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            QRInstructions().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        scan.setOnClickListener {
+            Navigation.findNavController(root).navigate(R.id.QRScanner)
+        }
+        return root
     }
 }
--- android_app/app/src/main/java/com/example/fitcom/QRScanner.kt
+++ android_app/app/src/main/java/com/example/fitcom/QRScanner.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
 
 class QRScanner : Fragment() {
 
@@ -13,6 +15,8 @@ class QRScanner : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_qr_scanner, container, false)
+        val root = inflater.inflate(R.layout.fragment_qr_scanner, container, false)
+
+        return root
     }
 }
