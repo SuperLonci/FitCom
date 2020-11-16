4409757632fd44293d9fcb445b8e313038f2f0a6
SuperLonci <55552282+SuperLonci@users.noreply.github.com>
2020-11-16 17:09:59 +0100

    add Fragments for Registration

--- android_app/app/src/main/java/com/example/fitcom/QRInstructions.kt
+++ android_app/app/src/main/java/com/example/fitcom/QRInstructions.kt
@@ -1,11 +1,10 @@
-package com.example.fitcom.ui.welcome
+package com.example.fitcom
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.fitcom.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -14,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [QRScanner.newInstance] factory method to
+ * Use the [QRInstructions.newInstance] factory method to
  * create an instance of this fragment.
  */
-class QRScanner : Fragment() {
+class QRInstructions : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +34,7 @@ class QRScanner : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_qr_scanner, container, false)
+        return inflater.inflate(R.layout.fragment_qr_instructions, container, false)
     }
 
     companion object {
@@ -45,12 +44,12 @@ class QRScanner : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment QRScanner.
+         * @return A new instance of fragment QRInstructions.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            QRScanner().apply {
+            QRInstructions().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
