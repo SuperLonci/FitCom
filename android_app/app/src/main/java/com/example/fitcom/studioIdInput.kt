b382c9fe14833d0b2e2e8b951a4a2f93a266d28a
Ian Helmrich <55527600+lordi-byte@users.noreply.github.com>
2020-11-17 16:41:00 +0100

    add materialacomponent and finish fragment_studio_id_input.xml

--- android_app/app/src/main/java/com/example/fitcom/studioIdInput.kt
+++ android_app/app/src/main/java/com/example/fitcom/studioIdInput.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.navigation.Navigation
+import com.google.android.material.textfield.TextInputLayout
 
 class studioIdInput : Fragment() {
 
@@ -13,6 +17,19 @@ class studioIdInput : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_studio_id_input, container, false)
+        val root = inflater.inflate(R.layout.fragment_studio_id_input, container, false)
+        val next = root.findViewById<Button>(R.id.next)
+
+        next.setOnClickListener {
+            val edtTextLayout = root.findViewById<TextInputLayout>(R.id.text_input_layout_id)
+            if(root.findViewById<EditText>(R.id.editText_id).text.length > 10) {
+                edtTextLayout.isErrorEnabled = true
+                edtTextLayout.error = "Diese StudioID ist ungültig."
+            }
+            else {
+                Navigation.findNavController(root).navigate(R.id.nameInput)
+            }
+        }
+        return root
     }
 }
