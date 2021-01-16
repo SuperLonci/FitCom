4d7bca65738ae7affe943578a0b7ed7fa242ba3d
Ian Helmrich <55527600+lordi-byte@users.noreply.github.com>
2021-01-16 16:58:12 +0100

    add a splash screen

---
+++ android_app/app/src/main/java/tech/fitcom/app/MainActivity.kt
@@ -0,0 +1,22 @@
+package tech.fitcom.app
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //hides the action bar in the loading screen
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            val intent = Intent(this@MainActivity, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
