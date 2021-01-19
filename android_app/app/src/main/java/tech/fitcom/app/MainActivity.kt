c68ad259f6fad968991687cc7f099f77adb3e48a
Ian Helmrich <55527600+lordi-byte@users.noreply.github.com>
2021-01-19 19:40:49 +0100

    add a stage for manual authorization and the welcome activity with the navigation xml

--- android_app/app/src/main/java/tech/fitcom/app/MainActivity.kt
+++ android_app/app/src/main/java/tech/fitcom/app/MainActivity.kt
@@ -5,8 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import tech.fitcom.app.registration.WelcomeActivity
 
 class MainActivity : AppCompatActivity() {
+
+    //constant for manual start HomeActivity or LoginActivity
+    val AUTHENTICATE = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,8 +21,15 @@ class MainActivity : AppCompatActivity() {
 
         //sets a timer for 3 seconds to show the logo in that time
         Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this@MainActivity, HomeActivity::class.java)
-            startActivity(intent)
+
+            if(AUTHENTICATE) {
+                val intent = Intent(this@MainActivity, WelcomeActivity::class.java)
+                startActivity(intent)
+            }
+            else {
+                val intent = Intent(this@MainActivity, HomeActivity::class.java)
+                startActivity(intent)
+            }
             finish()
         }, 3000)
     }
---
+++ android_app/app/src/main/java/tech/fitcom/app/registration/WelcomeActivity.kt
@@ -0,0 +1,12 @@
+package tech.fitcom.app.registration
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import tech.fitcom.app.R
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
