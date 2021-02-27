45076c6803d1c816e4355e92f4a6945ed85a8139
Ian Helmrich <ian.helmrich@gmail.com>
2021-02-27 17:39:02 +0100

    fix the bottomNavBar and delete some redundant files

--- android_app/app/src/main/java/tech/fitcom/app/home/HomeActivity.kt
+++ android_app/app/src/main/java/tech/fitcom/app/home/HomeActivity.kt
@@ -2,6 +2,7 @@ package tech.fitcom.app.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import androidx.navigation.Navigation.findNavController
@@ -11,23 +12,21 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import tech.fitcom.app.R
+import tech.fitcom.app.registration.EmailInput
+import tech.fitcom.app.registration.NewUser
+import tech.fitcom.app.registration.QRInstructions
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        val navView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        val navController = findNavController(R.id.fragment)
+        //switch the title in the appBar for each fragment
+        val appBarConfig = AppBarConfiguration(setOf(R.id.homeFragement,R.id.trainingFragment,R.id.profileFragment))
+        setupActionBarWithNavController(navController, appBarConfig)
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_training, R.id.navigation_profile
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
